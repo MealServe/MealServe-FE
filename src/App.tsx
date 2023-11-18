4574880a08111879b4c884ce699a550bc9b1ba10
Joonyoung Hong <hjunyoung10@gmail.com>
2023-11-18 15:05:15 +0900

    feat: Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import StoreDetail from './pages/StoreDetail';
+import NotFound from './pages/NotFound';
+import Layout from './components/Layout';
+import MenuDetail from './pages/MenuDetail';
+import RegisterStore from './pages/RegisterStore';
+import RegisterMenu from './pages/RegisterMenu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/stores/:storeId" element={<StoreDetail />} />
+        <Route path="/stores/register" element={<RegisterStore />} />
+        <Route path="/stores/menus/register" element={<RegisterMenu />} />
+        <Route path="/stores/:storeId/menus/:menuId" element={<MenuDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
