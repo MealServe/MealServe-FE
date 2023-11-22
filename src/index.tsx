52affa3337ae13a7afb63e74ff19d2d65e6953cb
Joonyoung Hong <hjunyoung10@gmail.com>
2023-11-22 15:56:33 +0900

    feat: Update index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,20 @@ import GlobalStyle from './styles/GlobalStyle';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 );
