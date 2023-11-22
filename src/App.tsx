74b92becae5dbc1b93e448d81a89dc9826ade215
Joonyoung Hong <hjunyoung10@gmail.com>
2023-11-22 15:56:47 +0900

    feat: Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,38 @@ import Signup from './pages/Signup';
 import StoreDetail from './pages/StoreDetail';
 import NotFound from './pages/NotFound';
 import Layout from './components/Layout';
-import MenuDetail from './pages/MenuDetail';
 import RegisterStore from './pages/RegisterStore';
 import RegisterMenu from './pages/RegisterMenu';
+import Orders from './pages/Orders';
+import HttpClient from './network/http';
+import AuthService from './service/authService';
+import StoreService from './service/storeService';
 
+const baseURL: string = process.env.REACT_APP_BASE_URL as string;
+const httpClient = new HttpClient(baseURL);
+const authService = new AuthService(httpClient);
+const storeService = new StoreService(httpClient);
+
+// TODO: Nested Route로 refactor
 function App() {
+  // console.log(baseURL);
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Home storeService={storeService} />} />
+        <Route path="/login" element={<Login authService={authService} />} />
+        <Route path="/signup" element={<Signup authService={authService} />} />
         <Route path="/stores/:storeId" element={<StoreDetail />} />
-        <Route path="/stores/register" element={<RegisterStore />} />
-        <Route path="/stores/menus/register" element={<RegisterMenu />} />
-        <Route path="/stores/:storeId/menus/:menuId" element={<MenuDetail />} />
+        <Route
+          path="/stores/register"
+          element={<RegisterStore storeService={storeService} />}
+        />
+        <Route
+          path="/stores/menus/register"
+          element={<RegisterMenu storeService={storeService} />}
+        />
+        {/* <Route path="/stores/:storeId/menus/:menuId" element={<MenuDetail />} /> */}
+        <Route path="/orders" element={<Orders />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
