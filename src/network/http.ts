0cb9ab386ceaad792c40b3a31f7fb9dd6cb50bde
Joonyoung Hong <hjunyoung10@gmail.com>
2023-11-23 20:26:45 +0900

    fix: Add Access-Control-Allow_Origin to fetch headers

--- src/network/http.ts
+++ src/network/http.ts
@@ -14,6 +14,7 @@ export default class HttpClient {
       credentials: 'include',
       headers: {
         // 'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': `${process.env.REACT_APP_BASE_URL}`,
         ...options.headers,
       },
     });
